08f0c34927c0f6cc5f10d8f253e6a8da93ee08fa
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-03-27 17:45:14 -0400

    update fn name

--- 01_hello_world/tests/prints-hello-world.rs
+++ 01_hello_world/tests/prints-hello-world.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 use std::str;
 
 #[test]
-fn works_when_passed_two_args() -> Result<(), Box<dyn std::error::Error>> {
+fn works() -> Result<(), Box<dyn std::error::Error>> {
     let output_bytes = Command::cargo_bin("hello_world")?.output().unwrap().stdout;
 
     let output_str = match str::from_utf8(&output_bytes) {
