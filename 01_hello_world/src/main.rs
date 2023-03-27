2dafe871b8d67a7663657f7fe1e53bec66ca26b1
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-03-27 17:56:38 -0400

    proper docstring comments

--- 01_hello_world/src/main.rs
+++ 01_hello_world/src/main.rs
@@ -1,12 +1,11 @@
-/*
-   The simplest of simple for Rust programs!
 
-   Create a project like this with the command: `cargo new`
-
-   Run this project with the command: `cargo run`
-
-   Run the test with: `cargo test`
-*/
+//!   The simplest of simple for Rust programs!
+//!
+//!   Create a project like this with the command: `cargo new`
+//!
+//!   Run this project with the command: `cargo run`
+//!
+//!   Run the test with: `cargo test`
 
 fn main() {
     println!("Hello, world!");
--- 02_arg_parse/src/main.rs
+++ 02_arg_parse/src/main.rs
@@ -1,7 +1,25 @@
+//! Welcome to the Hello, World project!
+//!
+//! Notice the comments with an extra exclamation mark. This
+//! allows us to create idiomatic Rust module or crate level documentation.
+//! - Bullet point
+//! - [`Some Link`]
+//!  This program can be run with the command: `cargo run`
+//!
+//!  This cli tool requires two arguments, a _pattern_ and a _path_.
+//!
+//!  Note that the pattern argument can be any string but the path must be a valid file or folder path.
+//!
+//!  An example that runs cleanly: `cargo run -- foo .`
+//!
+//! [`Link Example`]: http://thatwaseasy.example.com
+
 /*
    This program can be run with the command: `cargo run`
 
    Note that it requires two arguments, a _pattern_ and a _path_.
+   
+   The 
 
    An example that runs cleanly: `cargo run -- foo .`
 */
