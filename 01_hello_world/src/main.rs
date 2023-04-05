0d0eebcf7bcf2dbd56c144fd050d1fc6325001ea
Jim Lynch <mrdotjim@gmail.com>
2023-04-04 20:17:34 -0400

    Update main.rs

--- 01_hello_world/src/main.rs
+++ 01_hello_world/src/main.rs
@@ -1,12 +1,3 @@
-
-//!   The simplest of simple for Rust programs!
-//!
-//!   Create a project like this with the command: `cargo new`
-//!
-//!   Run this project with the command: `cargo run`
-//!
-//!   Run the test with: `cargo test`
-
 fn main() {
     println!("Hello, world!");
 }
