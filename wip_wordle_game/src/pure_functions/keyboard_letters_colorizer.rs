d1f21b6cc4550e38a3f980f70d6716ce4d508d71
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-05-15 16:09:44 -0400

    fixes text movement wonkiness

--- wip_wordle_game/src/pure_functions/keyboard_letters_colorizer.rs
+++ wip_wordle_game/src/pure_functions/keyboard_letters_colorizer.rs
@@ -1,17 +1,70 @@
-use console::style;
-
 use crate::data_elements::data::GuessState;
+use termion::{color, style};
 
 pub fn get_colored_keyboard_letters(text: &str, guessed_state: &GuessState) -> String {
     match guessed_state {
         GuessState::InWordFoundLocation(_) => {
-            style(text.to_string()).magenta().on_green().to_string()
+            format!(
+                "{}{}{}{}",
+                color::Fg(color::Rgb(0, 1, 2)),
+                color::Bg(color::Rgb(0, 1, 2)),
+                text,
+                style::Reset
+            )
+
+            // style(text.to_string()).magenta().on_green().to_string()
+            // text.to_string()
+            // text_style::StyledString::plain("test".to_string()).bold().style({fg})
+            //     StyledString::styled(text.to_string(), text_style::Style {
+            //     fg: Some(text_style::AnsiColor::Magenta.dark()),
+            //     bg: Some(text_style::Color::Rgb { r: 20, g: 20, b: 20 }),
+            //     effects: None as text_style::Style
+            //     // style(text.to_string()).magenta().on_green().to_string()
+            // }
         }
-        GuessState::InWordUnknownLocation => {
-            style(text.to_string()).magenta().on_yellow().to_string()
+        GuessState::InWordUnknownLocation =>
+        // text.to_string(),
+        // {
+        //     style(text.to_string()).magenta().on_green().to_string()
+        // }
+        {
+            format!(
+                "{}{}{}{}",
+                color::Fg(color::Rgb(0, 1, 2)),
+                color::Bg(color::Rgb(0, 1, 2)),
+                text,
+                style::Reset
+            )
         }
-        GuessState::GuessedNotInWord => style(text.to_string()).black().on_black().to_string(),
-        GuessState::Unguessed => style(text.to_string()).black().on_white().to_string(),
+
+        // style(text.to_string()).magenta().on_yellow().to_string()
+        GuessState::GuessedNotInWord =>
+        // style(text.to_string()).magenta().on_green().to_string(),
+        {
+            format!(
+                "{}{}{}{}",
+                color::Fg(color::Rgb(0, 1, 2)),
+                color::Bg(color::Rgb(0, 1, 2)),
+                text,
+                style::Reset
+            )
+        }
+
+        // text.to_string(),
+        // style(text.to_string()).black().on_black().to_string(),
+        GuessState::Unguessed =>
+        // style(text.to_string()).magenta().on_green().to_string(),
+        {
+            format!(
+                "{}{}{}{}",
+                color::Fg(color::Rgb(0, 1, 2)),
+                color::Bg(color::Rgb(0, 1, 2)),
+                text,
+                style::Reset
+            )
+        } // style(text.to_string()).black().on_white().to_string(),
+
+          // text.to_string()
     }
 }
 
--- wip_wordle_game/src/side_effect_inputs/text_input_handler.rs
+++ wip_wordle_game/src/side_effect_inputs/text_input_handler.rs
@@ -20,41 +20,10 @@ pub fn get_guess_from_user(
         .iter()
         .map(|(key, guessed_state)| get_colored_keyboard_letters(key, guessed_state))
         .collect::<Vec<String>>()
-        .join(", ");
-
-    // let _hardcodeded_letters_remaining = format!(
-    //     "{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}",
-    //     get_color_for_keyboard_letter("a", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("b", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("c", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("d", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("e", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("f", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("g", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("h", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("i", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("j", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("k", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("l", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("m", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("n", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("o", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("p", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("q", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("r", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("s", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("t", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("u", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("v", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("w", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("x", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("y", &prev_letters_guessed)?,
-    //     get_color_for_keyboard_letter("z", &prev_letters_guessed)?,
-    // );
+        .join(" ");
 
     Ok(Text::new("➠")
         .with_help_message(&styled_letters_remaining)
-        // .with_help_message(&hardcodeded_letters_remaining)
         .with_validator(required!())
         .with_validator(length!(letters_in_word))
         .with_validator(move |word: &str| {
